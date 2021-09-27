90212f39ab0505cb44b2b1a0a39637fcef7504c1
DATND <dinhdat.k51@gmail.com>
2021-09-27 23:44:33 +0700

    updata source code html/css base

---
+++ owlcarousel/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  // $(".owl-carousel").owlCarousel();
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:16,
+	    nav:false,
+	    responsive:{
+	        0:{
+	            items:,
+	        },
+	        600:{
+	            items:3,
+	        },
+	        1000:{
+	            items:3,
+	        }
+	    }
+	})
+});
+
